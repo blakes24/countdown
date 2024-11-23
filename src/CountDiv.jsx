cd4651c2d914fe45be5b018a6ec5f7b43f57b5b5
blakes24 <blakes24@gmail.com>
2024-11-23 15:27:46 -0800

    Style countdown cards

--- src/CountDiv.jsx
+++ src/CountDiv.jsx
@@ -1,3 +1,12 @@
-export const CountDiv = ({ count }) => {
-  return <div>{count}</div>;
+import "./CountDiv.css";
+
+export const CountDiv = ({ count, label }) => {
+  const num = count.toString().padStart(2, "0");
+
+  return (
+    <div className="count-card">
+      <div className="count">{num}</div>
+      <div>{label}</div>
+    </div>
+  );
 };
